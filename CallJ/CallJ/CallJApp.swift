74760ae8c0d73b475c2c7e8cfe0978419396b799
TheWhitePigeon <158068409+TheWhitePigeon@users.noreply.github.com>
2024-12-15 20:58:47 +0100

    Add files via upload

---
+++ CallJ/CallJ/CallJApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallJApp.swift
+//  CallJ
+//
+//  Created by Zhanet Nikolovska on 13.12.24.
+//
+
+import SwiftUI
+
+@main
+struct CallJApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CallJ/CallJTests/CallJTests.swift
@@ -0,0 +1,17 @@
+//
+//  CallJTests.swift
+//  CallJTests
+//
+//  Created by Zhanet Nikolovska on 13.12.24.
+//
+
+import Testing
+@testable import CallJ
+
+struct CallJTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
